943bcb8fa1091c717273c86b6452cc997b983019
minsik <a184562@gmail.com>
2023-07-30 22:12:26 +0900

    Unity Toy Project(Vampire Survivor Like) 3rd Day

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,11 +9,15 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -21,6 +25,16 @@ public class Player : MonoBehaviour
     {
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0 ;
+        }
     }
 
     void OnMove(InputValue value)
---
+++ Assets/Script/Reposition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
