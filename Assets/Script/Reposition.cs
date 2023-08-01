444316f21b4a36c1b4ea16df5fe71ea5c9f50586
minsik <a184562@gmail.com>
2023-08-01 21:58:53 +0900

    Unity Toy Project(Vampire Survivor Like) 4th Day

--- Assets/Script/Reposition.cs
+++ Assets/Script/Reposition.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+
     void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if(!collision.CompareTag("Area"))
+        {
+            return;
+        }
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 mtPos = transform.position;
+        float diffx = Mathf.Abs(playerPos.x - mtPos.x);
+        float diffy = Mathf.Abs(playerPos.y - mtPos.y);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(diffx > diffy)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffx < diffy)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+
+                break;
+
+        }
     }
 }
